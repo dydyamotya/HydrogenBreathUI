d14d0e29ac8795001798b9622c849c0b065da8a5
dydyamotya <matderevenayv@yandex.ru>
2023-08-21 18:48:56 +0300

    heater params to saved file

--- plot_widget.py
+++ plot_widget.py
@@ -5,6 +5,8 @@ import pathlib
 import json2html
 import typing
 import datetime
+import json
+
 if typing.TYPE_CHECKING:
     from device import HeaterParamsTuple, HeaterCalTransformTuple
 
@@ -34,7 +36,7 @@ class PlotWidget(pg.PlotWidget):
         self.getPlotItem().setLogMode(y=False)
         self.getPlotItem().setLabel("left", "Temperature", units="°C")
         self.getPlotItem().setLabel("bottom", "Voltage", units="V")
-        data = np.hstack([voltages, temperatures, ms_voltages, ms_temperatures]).reshape((2, -1)).T
+        data = np.hstack([voltages, temperatures]).reshape((2, -1)).T
         data2 = np.hstack([ms_voltages, ms_temperatures]).reshape((2, -1)).T
         data3 = np.hstack([ms_voltages_recalc, ms_temperatures_recalc]).reshape((2, -1)).T
         data4 = np.hstack([voltages_cal, temperatures_cal]).reshape((2, -1)).T
@@ -42,6 +44,10 @@ class PlotWidget(pg.PlotWidget):
         np.savetxt(pathlib.Path.cwd() / "ms_version{}.csv".format(datetime.datetime.now().isoformat().replace(":", ".")), data2)
         np.savetxt(pathlib.Path.cwd() / "ms_version_recalc{}.csv".format(datetime.datetime.now().isoformat().replace(":", ".")), data3)
         np.savetxt(pathlib.Path.cwd() / "device_cal_version{}.csv".format(datetime.datetime.now().isoformat().replace(":", ".")), data4)
+        if heater_params is not None:
+            with (pathlib.Path.cwd() / "heater_params{}.json".format(datetime.datetime.now().isoformat().replace(":", "."))).open("w") as fd:
+                json.dump(heater_params._asdict(), fd)
+
         line1 = self.plot(x=voltages, y=temperatures, pen=pg.mkPen("green"))
         line2 = self.plot(x=ms_voltages, y=ms_temperatures, pen=pg.mkPen("blue"))
         line3 = self.plot(x=ms_voltages_recalc, y=ms_temperatures_recalc, pen=pg.mkPen("red"))
